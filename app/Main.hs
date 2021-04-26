5eb89633a7f039a5517529c6ed8d3ba40fc7e754
Benjamin Reigner <benjamin.reigner@epitech.eu>
2021-04-26 21:39:46 +0200

    feat: add argument parsing
    The parsing of arguments is done and they are stored in a data structure
    for more convenience on its use
    For now the main prints the data if the arguments are valid or
    "Invalid Arguments" if the arguments aren't valid

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,39 @@
 module Main where
 
-import Lib
+import Text.Read
+import System.Environment
+import System.Exit
+import Control.Monad
+
+data Input = Input {
+    colorsNumber :: Int,
+    convergenceLimit :: Float,
+    file :: String
+} deriving (Show)
+
+readPositiveInt :: String -> Maybe Int
+readPositiveInt str = case readMaybe str of
+        Just nb ->  if nb >= 0
+                    then Just nb
+                    else Nothing
+        Nothing -> Nothing
+
+
+parseArgs :: [String] -> Maybe Input
+parseArgs [colors, convergence, file] = case readPositiveInt colors of
+    Just colorsNb -> case readMaybe convergence :: Maybe Float of
+        Just convergenceNb -> Just Input {
+                colorsNumber = colorsNb,
+                convergenceLimit = convergenceNb,
+                file = file
+            }
+        Nothing -> Nothing
+    Nothing -> Nothing
+parseArgs _ = Nothing
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    case parseArgs args of
+        Just input -> print input
+        Nothing -> putStrLn "Invalid Arguments" >> exitWith (ExitFailure 84)
